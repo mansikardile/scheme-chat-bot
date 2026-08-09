4eefebc3df8f60dbf918c840066b58e282598ca0
Hurditya M <hurditya.mangalapelly24@vit.edu>
2026-08-09 13:40:38 +0530

    add schemes saving function to bulk download json

--- src/scheme_sathi/scraper.py
+++ src/scheme_sathi/scraper.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import json
 import requests
 
 X_API_KEY = "tYTy5eEhlu9rFjyxuCr7ra7ACp4dv1RH8gWuHTDc"
@@ -41,3 +43,41 @@ def fetch_scheme_details(slug):
     r = session.get(f"{BASE_URL}/schemes/v6/public/schemes", params=params)
     r.raise_for_status()
     return r.json()["data"]
+
+def save_all_schemes():
+    total = get_total_schemes()
+
+    saved = 0
+    skipped = 0
+    start_index = 0
+
+    schemes_dir = Path(__file__).resolve().parents[2] / "schemes"
+    schemes_dir.mkdir(parents=True, exist_ok=True)
+    
+    print(f"Total Schemes available: {total}")
+
+    while start_index < total:
+        schemes = search_schemes(start_index)
+
+        if not schemes:
+            print("No more schemes returned.")
+            break
+
+        for scheme in schemes:
+            slug = scheme["fields"]["slug"]
+            details = fetch_scheme_details(slug)
+            filepath = schemes_dir / f"{slug}.json"
+
+            if filepath.exists():
+                skipped += 1
+                continue
+            
+            filepath.write_text(json.dumps(details, ensure_ascii=False, indent=2))
+            saved += 1
+            print(f"Saved {slug}")
+            
+        start_index += len(schemes)
+
+
+if __name__ == "__main__":
+    save_all_schemes()
